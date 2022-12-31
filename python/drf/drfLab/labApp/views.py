a087e7c9269338563dcebf5062280303c16e8639
Pedro <pedro.saravia.q@uni.pe>
2022-12-31 17:05:11 -0500

    function and class views

--- python/drf/drfLab/labApp/views.py
+++ python/drf/drfLab/labApp/views.py
@@ -5,7 +5,19 @@ from rest_framework.decorators import api_view
 
 # Create your views here.
 
+# * Function-based views
+
 
 @api_view()
 def books(request):
     return Response("list of books", status=status.HTTP_200_OK)
+
+
+# * Class-based views
+
+
+class Orders():
+    @staticmethod
+    @api_view()
+    def listOrders(request):
+        return Response({"message": "list of orders"}, 200)
