684b686637058db7be88be7f05786ba750af3a46
Pedro <pedro.saravia.q@uni.pe>
2022-12-31 17:49:02 -0500

    class extending viewsets.ViewSet

--- python/drf/drfLab/labApp/urls.py
+++ python/drf/drfLab/labApp/urls.py
@@ -4,5 +4,16 @@ from . import views
 urlpatterns = [
     path("books", views.books),
     path("orders", views.Orders.listOrders),
-    path("items/<int:pk>", views.BookView.as_view())
+    path("items/<int:pk>", views.BookView.as_view()),
+    path('books2', views.BookView2.as_view(
+        {
+            'get': 'list',
+            'post': 'create',
+        }
+    )),
+    path('books2/<int:pk>', views.BookView2.as_view(
+        {
+            'get': 'retrieve',
+        }
+    ))
 ]
--- python/drf/drfLab/labApp/views.py
+++ python/drf/drfLab/labApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 
@@ -30,3 +30,17 @@ class Orders():
 class BookView(APIView):
     def get(self, request, pk):
         return Response({"message": "single book with id " + str(pk)}, status.HTTP_200_OK)
+
+
+# * Class extending "viewsets.ViewSet"
+
+
+class BookView2(viewsets.ViewSet):
+    def list(self, request):
+        return Response({"message": "All books"}, status.HTTP_200_OK)
+
+    def create(self, request):
+        return Response({"message": "Creating a book"}, status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        return Response({"message": "Displaying a book"}, status.HTTP_200_OK)
