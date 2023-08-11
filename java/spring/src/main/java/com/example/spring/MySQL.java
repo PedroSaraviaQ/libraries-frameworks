14ae7f4c4e0ee0bb282c94d911b298e671521d54
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-08-11 15:38:30 -0500

    add mysql instructions

---
+++ java/spring/src/main/java/com/example/spring/MySQL.java
@@ -0,0 +1,14 @@
+package com.example.spring;
+
+//* In order to work with MySQL, you need to add the following dependencies:
+//* spring-boot-starter-data-jpa, mysql-connector-j (not java, only j), spring-boot-starter-validation
+
+//* In the "application.properties" file, you need to add the following:
+//* spring.datasource.url=jdbc:mysql://(host):(port)/(database_name)
+//* spring.datasource.username=(username)
+//* spring.datasource.password=(password)
+//* spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+//* spring.jpa.hibernate.ddl-auto=update
+
+public class MySQL {
+}
