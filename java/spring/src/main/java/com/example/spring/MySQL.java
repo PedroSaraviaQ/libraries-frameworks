6d1f2ea97d902f078efa6870b168a9490a8f1b3b
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-08-13 10:45:32 -0500

    add mysql suggestion

--- java/spring/src/main/java/com/example/spring/MySQL.java
+++ java/spring/src/main/java/com/example/spring/MySQL.java
@@ -3,6 +3,8 @@ package com.example.spring;
 //* In order to work with MySQL, you need to add the following dependencies:
 //* spring-boot-starter-data-jpa, mysql-connector-j (not java, only j), spring-boot-starter-validation
 
+//! Prior to assigning the database, you need to create it somehow in MySQL.
+
 //* In the "application.properties" file, you need to add the following:
 //* spring.datasource.url=jdbc:mysql://(host):(port)/(database_name)
 //* spring.datasource.username=(username)
