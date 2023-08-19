b8ca6e279d3cd22aaad09ef87e6dcff6952bc7e2
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-08-18 22:59:49 -0500

    add one to one relation

---
+++ java/spring/src/main/java/com/example/spring/relations/onetoone/ArabicNumeral.java
@@ -0,0 +1,20 @@
+package com.example.spring.relations.onetoone;
+
+import com.example.spring.models.SuperModel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "arabics")
+public class ArabicNumeral extends SuperModel {
+
+    //* We start with this model that will depend on the other one.
+
+    //* The "@OneToOne" annotation will only take the lazy fetch type.
+    @OneToOne(fetch = FetchType.LAZY)
+
+    //* The "@JoinColumn" annotation will have the name of the column that will reference the other model.
+    @JoinColumn(name = "roman_id")
+
+    //* Then the field of the other model, its variable name will be referenced by the other model.
+    private RomanNumeral roman;
+}
