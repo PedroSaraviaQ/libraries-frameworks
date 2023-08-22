56dde213ad84d8940b4577657a8eb73db1cbd5e3
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-08-22 16:14:10 -0500

    reduce routes code

--- java/spring/src/main/java/com/example/spring/Routes.java
+++ java/spring/src/main/java/com/example/spring/Routes.java
@@ -26,28 +26,25 @@ public class Routes {
     //* As well as the others: @PostMapping, @PutMapping, @DeleteMapping, @PatchMapping
 }
 
-//* If you want to group the routes, you can use the @RequestMapping annotation in the class.
 @RestController
-//* Sometimes you can omit the "/" in the routes, but it's better to use it.
-// @RequestMapping("api")
+//* If you want to have a base route for all the routes in a class, you can use the @RequestMapping annotation.
 @RequestMapping("/api")
 class ApiRoutes {
 
-    //* So this route would be "/api/users"
-    @GetMapping("/users")
-    public String menu() {
-        return "Hello users!";
+    //! Be careful when using "" and "/" in the routes, because they are different in some cases.
+    @GetMapping("")
+    public String noSlash() {
+        return "Without slash!";
+    }
+
+    @GetMapping("/")
+    public String slash() {
+        return "With slash. See the difference?";
     }
 
     //* You can also use variables in the routes.
     //! Spring handles the data type conversion automatically, unlike Express.js.
     //* If the path variable matches the method parameter, you can omit the argument name.
-    @GetMapping("/item/{id}")
-    public String item(@PathVariable int id) {
-        return "Item " + id;
-    }
-
-    //* You can also omit them for multiple path variables.
     @GetMapping("/item/{category}/{id}")
     public String items(@PathVariable String category, @PathVariable int id) {
         return category + " item " + id;
@@ -67,16 +64,7 @@ class ApiRoutes {
     }
 
     //* When you want the parameter to be optional, you can use the required argument.
-    @GetMapping("/user")
-    public String user(@RequestParam(required = false) String name) {
-        if (name == null) {
-            return "Hello user!";
-        }
-        return "Hello " + name;
-    }
-
-    //* But when those values are optional, you should use wrappers instead of primitives
-    //* to take into account the "null" cases.
+    //* You should use wrappers instead of primitives to take into account the "null" cases.
     @GetMapping("/product")
     public String product(@RequestParam(required = false) Integer id) {
         if (id == null) {
@@ -86,22 +74,8 @@ class ApiRoutes {
     }
 
     //! So this would throw an error because primitives can't be null.
-    // @GetMapping("/products")
-    // public String products(@RequestParam(required = false) int id) {
-    //     if (id == null) {
-    //         return "Hello product!";
-    //     }
-    //     return "Hello product " + id + "!";
-    // }
-
-    //! Be careful when using "" and "/" in the routes, because they are different in some cases.
-    @GetMapping("")
-    public String noSlash() {
-        return "Without slash!";
-    }
-
-    @GetMapping("/")
-    public String slash() {
-        return "With slash. See the difference?";
+    @GetMapping("/products")
+    public String products(@RequestParam(required = false) int id) {
+        return "Hello product " + id + "!";
     }
 }
