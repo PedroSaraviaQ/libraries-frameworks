3c4c8fa58de51dfe0701ee7747500de955167608
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-09-04 14:24:35 -0500

    modify oneToMany comments

--- java/spring/src/main/java/com/example/spring/relationships/oneToMany/Dojo.java
+++ java/spring/src/main/java/com/example/spring/relationships/oneToMany/Dojo.java
@@ -1,4 +1,4 @@
-package com.example.spring.relationships.onetomany;
+package com.example.spring.relationships.oneToMany;
 
 import com.example.spring.models.SuperModel;
 import jakarta.persistence.Entity;
@@ -12,9 +12,9 @@ import java.util.List;
 @Table(name = "dojos")
 public class Dojo extends SuperModel {
 
-    //* Here we change the one-to-one to the "@OneToMany" annotation, and don't include the cascade type.
+    //* "@OneToMany" doesn't include the cascade type, and precedes the collection.
     @OneToMany(mappedBy = "dojo", fetch = FetchType.LAZY)
 
-    //* And instead of only one field, we have a list of the other model.
+    //* Collection of linked objects of the other model.
     private List<Ninja> ninjas;
 }
---
+++ java/spring/src/main/java/com/example/spring/relationships/oneToMany/Ninja.java
@@ -0,0 +1,18 @@
+package com.example.spring.relationships.oneToMany;
+
+import com.example.spring.models.SuperModel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ninjas")
+public class Ninja extends SuperModel {
+
+    //* "@ManyToOne" also takes the lazy fetch type, and precedes "@JoinColumn".
+    @ManyToOne(fetch = FetchType.LAZY)
+
+    //* Tne column name for the foreign key in the database.
+    @JoinColumn(name = "dojo_id")
+
+    //* The linked object of the other model.
+    private Dojo dojo;
+}
